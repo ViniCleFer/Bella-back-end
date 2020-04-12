c02e3cd15483e6e43c4a80e115736e79a917fbba
unknown <viniciusclementefernandes@gmail.com>
2020-04-12 01:02:05 -0300

    Retorno Subscribe no Create

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -5,9 +5,12 @@ module.exports = {
   async index(request, response) {
     const {user_id} = request.params;
 
-    const programs = await Subscribe.find({user: user_id});
-
-    //console.log(programs);
+    const subscribes = await Subscribe.find({user: user_id});
+  
+    return response.json({
+      subscribes
+    });
+  
    
   },
 
@@ -17,18 +20,21 @@ module.exports = {
 
     const programs = await Program.findById(program_id);
 
+    console.log(program_id);
+
     const { professionals, name,  image, public, description} = programs;
 
     const subscribe = await Subscribe.create({
       user: user_id,
-      programs: {
-        professionals,
-        name,
-        image,
-        public,
-        description
-      }
+      program_id,
+      professionals,
+      name,
+      image,
+      public,
+      description
     });
+
+    console.log(subscribe);
 
     const { _id} = subscribe;
     
@@ -36,7 +42,8 @@ module.exports = {
       subscribe: {
         id: _id,
         user: user_id,
-        programs: {
+        program: {
+          id: program_id,
           professionals,
           name,
           image,
